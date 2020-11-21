dd018801e198dd7cb40d619a79a14ed8ecc23077
IssaDia <issadiapro@gmail.com>
2020-11-21 16:55:53 +0100

    First deploy

---
+++ components/post.js
@@ -0,0 +1,12 @@
+function Post({ alt, date, image, title, url }) {
+  return (
+    <div className="container">
+      <div className="text">
+        <h2>{title}</h2>
+      </div>
+      <style jsx>{``}</style>
+    </div>
+  )
+}
+
+export default Post
