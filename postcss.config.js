8d4e322a19889d70867c59bb6ba249a4d0f5dcbe
IssaDia <issadiapro@gmail.com>
2020-11-23 02:46:05 +0100

    New build after changing tailwind CSS try 4

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,14 @@
 module.exports = {
   plugins: [
     'tailwindcss',
-    'postcss-preset-env',
-    {
-      stage: 1,
-      features: {
-        'focus-within-pseudo-class': false
+    [
+      'postcss-preset-env',
+      {
+        stage: 1,
+        features: {
+          'focus-within-pseudo-class': false
+        }
       }
-    }
+    ]
   ]
 }
